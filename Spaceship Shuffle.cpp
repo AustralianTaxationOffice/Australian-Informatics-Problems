9bb28d9ef0c209839e7e3dae0ea48cc0a18cd1f6
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-08-06 14:56:24 +0800

    Solve Spaceship Shuffle

--- Spaceship Shuffle.cpp
+++ Spaceship Shuffle.cpp
@@ -1,52 +1,30 @@
-#include <iostream>
-#include <cmath>
-#include <numeric>
+#include<bits/stdc++.h>
 using namespace std;
-#define speed() \
-ios::sync_with_stdio(false); \
-cin.tie(0)
-#define piss(name) \
-speed(); \
-freopen(#name "in.txt","r",stdin); \
-freopen(#name "out.txt","w",stdout)
-#define int long long
-#define umap std::unordered_map
-#define ummap std::unordered_multimap
-#define uset std::unordered_set
-#define umset std::unordered_multiset
-#define mmap std::multimap
-#define mset std::multiset
-signed main()
-{
-    piss(space);
-    int n;cin>>n;
-    int ci[n],cd[n];
-    int cm[n],psa[n];
-    for (int i=0;i<n;i++){
-        int iv;cin>>iv;
-        ci[i]=iv;
+
+int main() {
+    freopen("spacein.txt", "r", stdin);
+    freopen("spaceout.txt", "w", stdout);
+
+    int n; cin >> n;
+    vector<int> a(n), b(n);
+    for (int& x : a) cin >> x;
+    for (int& x : b) cin >> x;
+
+    vector<long long> diff(n), diffPrefixSum(n);
+
+    long long tmpSum = 0;
+    for (int i = 0; i < n; i++) {
+        diff[i] = a[i] - b[i];
+        diffPrefixSum[i] = (tmpSum += diff[i]);
     }
-    for (int i=0;i<n;i++){
-        int iv;cin>>iv;
-        cd[i]=iv;
+
+    sort(diffPrefixSum.begin(), diffPrefixSum.end());
+
+    const int HALF_n = (n - 1) >> 1;
+    long long ans = 0, sub = diffPrefixSum[HALF_n];
+    for (int i = 0; i < n; i++) {
+        ans += abs(diffPrefixSum[i] - sub);
     }
-    for (int i=0;i<n;i++){
-        cm[i]=ci[i]-cd[i];
-    }
-    int sm=0;int sub=-2e9;
-    for (int i=0;i<n;i++){
-        sm+=cm[i];
-        psa[i]=sm;
-        //cout<<psa[i]<<' ';
-    }
-    //cout<<'\n';
-    int cr=0;
-    sub=(psa[0]-psa[n-1])/2ll;
-    for (int i=0;i<n;i++){
-        psa[i]-=sub;
-        cr+=abs(psa[i]);
-        //cout<<psa[i]<<' ';
-    }
-    //cout<<'\n';
-    cout<<cr<<'\n';
+
+    cout << ans << endl;
 }
