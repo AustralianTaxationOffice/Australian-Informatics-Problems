fcfb52b28ceb9750906bf86d8c8daf01d8e5b040
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-08-06 14:54:20 +0800

    Solve Baubles

--- Baubles.cpp
+++ Baubles.cpp
@@ -1,18 +1,38 @@
-#include <climits>
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#define ll long long
+#include<bits/stdc++.h>
 using namespace std;
-long long ro,bo,s,rp,bp;
-int main()
-{
-    freopen("baublesin.txt","r",stdin);
-    freopen("baublesout.txt","w",stdout);
-    cin>>ro>>bo>>s>>rp>>bp;
-    int cr=0;
-    if (ro>=rp&&bo>=bp){
-        cr=min(ro-rp,bo-bp)+1;
+
+int main() {
+    freopen("baublesin.txt", "r", stdin);
+    freopen("baublesout.txt", "w", stdout);
+
+    int red, blue; cin >> red >> blue;
+    int spare; cin >> spare;
+    int red_order, blue_order; cin >> red_order >> blue_order;
+
+    int ans;
+
+    if (red > red_order && blue > blue_order) {
+        if (red_order == 0) {
+            ans = blue - blue_order + spare;
+        } else if (blue_order == 0) {
+            ans = red - red_order + spare;
+        } else {
+            ans = min(red - red_order, blue - blue_order) + spare;
+        }
+
+    } else if (red < red_order && blue < blue_order) {
+        ans = spare - (red_order - red + blue_order - blue);
+    } else {
+        if (blue_order == 0) {
+            ans = spare - red_order + red;
+        } else if (red_order == 0) {
+            ans = spare - blue_order + blue;
+        } else {
+            ans = spare - max(red_order - red, blue_order - blue);
+        }
     }
-    cout<<cr<<'\n';
+
+    ans++; // we sabotage one more bauble in order to stop him from fulfilling the order
+
+    cout << max(ans, 0);
 }
